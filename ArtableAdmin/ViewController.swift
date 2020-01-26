8d50b75f9b7a2dbd924f18c2f17d040ae7c77cfd
Pooja <pooja3003k@gmail.com>
2020-01-26 11:54:55 +0530

    Initial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Pooja Kadam on 26/01/20.
+//  Copyright © 2020 Pooja Kadam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
